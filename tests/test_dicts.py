7b2bedc6b1a8f1f5debb57ca2c4fee4e32b6df72
AnnaKolmychek <a.kolmychek@gmail.com>
2023-05-02 01:14:48 +0300

    add new test for dicts/get_value: case wrong type

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -7,3 +7,8 @@ def test_get_val():
     assert dicts.get_val({'vcs': 'mercurial'}, 'not_key') == 'git'
     assert dicts.get_val({}, 'vcs', 'git') == 'git'
     assert dicts.get_val({}, 'vcs', 'bazaar') == 'bazaar'
+
+
+def test_get_val__wrong_type():
+    assert dicts.get_val([1, 2, 3], 'vcs') == 'git'
+
