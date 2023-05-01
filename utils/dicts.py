20514c19679f6098e99954c43d40c1ce4cddb75c
AnnaKolmychek <a.kolmychek@gmail.com>
2023-05-02 01:12:29 +0300

    add code func dicts/get_value

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,2 +1,15 @@
-def get_val(collection, key, default='git'):
-    pass
+# функции для работы со словарями
+
+def get_val(collection: dict, key, default='git'):
+    """
+    Возвращает значение по ключу. Если ключа нет, возвращает defoult
+    :param collection: словарь
+    :param key: ключ для поиска
+    :param default: дефолтное значение для возврата, если ключа в словаре нет
+    :return: значение в словаре по ключу
+    """
+
+    if key in collection.keys():
+        return collection[key]
+
+    return default
